496e036ba6013d9bb10e1b239cacc087de46b556
ali-sorkhi <ali.sorkhi92@gmail.com>
2022-09-11 08:27:49 +0430

    removing B-P from BE

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compress from "compression"; //compress response bodies for all requests
 import cors from "cors"; //to enable cross-origin resource sharing (CORS)
@@ -20,14 +19,12 @@ connectDB();
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
-
-app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
