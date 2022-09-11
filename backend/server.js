541fa9893c16227482b0a593165f2e180bac7b6e
ali-sorkhi <ali.sorkhi92@gmail.com>
2022-09-11 08:21:26 +0430

    adding some lib to BE

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compress from "compression"; //compress response bodies for all requests
+import cors from "cors"; //to enable cross-origin resource sharing (CORS)
+import helmet from "helmet"; //secure Express apps bysetting various HTTP headers
+
 //routes:
 import authRouter from "./routes/authRouter.js";
 
@@ -13,6 +19,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
 
 app.use(express.json());
 
